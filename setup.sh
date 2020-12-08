3b725d56a7d1ac2929f987bd592b7898957a43a8
iliasbaker <72841537+iliasbaker@users.noreply.github.com>
2020-12-08 17:23:57 +1300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,1 +1,19 @@
-echo "I hate you"
+# Update everything first
+sudo apt-get update
+
+# Customising grub font size
+
+
+
+
+
+# Spotify and spicetify
+curl -sS https://download.spotify.com/debian/pubkey_0D811D58.gpg | sudo apt-key add - 
+echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+sudo apt-get update
+sudo apt-get install spotify-client
+sudo chmod a+wr /usr/share/spotify
+sudo chmod a+wr /usr/share/spotify/Apps -R
+curl -fsSL https://raw.githubusercontent.com/khanhas/spicetify-cli/master/install.sh | sh
+
+# LWT
